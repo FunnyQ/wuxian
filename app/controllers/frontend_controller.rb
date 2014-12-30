2ee2caa3885a4707725e506325755e38e84848a0
FunnyQ <funnyq@funnyq.com>
2014-12-30 17:17:02 +0800

    [add] add diaries index route

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -3,4 +3,16 @@
     def index
     end
 
+    def diaries_index
+    end
+
+    def diary_show
+    end
+
+    def albums_index
+    end
+
+    def album_show
+    end
+
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :frontend, :only => [:index]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'frontend#index'
+  get '/users/tim_cook/diaries' => 'frontend#diaries_index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
