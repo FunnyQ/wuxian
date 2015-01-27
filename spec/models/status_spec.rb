5b19ac76cadab2287123ec340ddefe510bdddfeb
FunnyQ <funnyq@funnyq.com>
2015-01-27 10:59:07 +0800

    [add] using annotate

--- spec/models/status_spec.rb
+++ spec/models/status_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: statuses
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Status, :type => :model do
