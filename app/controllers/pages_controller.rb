408cc0c2a58f4f63c767abf07bd16ad5a724e675
FunnyQ <funnyq@funnyq.com>
2015-01-28 02:23:15 +0800

    [add] adding flash message and update status behavior

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
   # Global Index Page
   def index
-
+    @statuses = Status.get_latest
   end
 
 end
--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -3,11 +3,17 @@ class StatusesController < ApplicationController
   def create
     @status = Status.new(status_params)
     @status.user_id = current_user.id
+    @user = current_user
         respond_to do |format|
-          if @status.save
-            format.html { redirect_to root_path, notice: 'status was successfully created.' }
+          if params[:status][:content].include?("script") || params[:status][:content].include?("img")
+            format.html {render 'users/show', locals: {user: @user}}
+            flash[:alert] = "抱歉，內容不能包含不合法的 HTML 標籤"
           else
-            format.html { redirect_to root_path }
+            if @status.save
+              format.html { redirect_to user_path(current_user) }
+            else
+              format.html { redirect_to user_path(current_user), alert: "請檢查是否正確輸入動態內容" }
+            end
           end
         end
   end
