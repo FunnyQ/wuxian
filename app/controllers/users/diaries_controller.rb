31b065899d628a362ac6278ff01e7914571fc414
FunnyQ <funnyq@funnyq.com>
2015-01-28 18:42:33 +0800

    [add] adding delete function to user diary

--- app/controllers/users/diaries_controller.rb
+++ app/controllers/users/diaries_controller.rb
@@ -27,12 +27,39 @@ class Users::DiariesController < ApplicationController
     @user = User.find(params[:user_id])
     @diary = @user.diaries.new(diary_params)
     if @diary.save
-      redirect_to user_diaries_path(@user)
+      redirect_to user_diaries_path
       flash[:notice] = "成功寫下一篇日記囉！"
     else
       render :new
       flash[:alert] = "Oops，請您檢查日記欄位後再試一次 :P"
     end
+  end
+
+  def edit
+    @diary = Diary.find(params[:id])
+    @user = @diary.user
+    if @user != current_user
+      redirect_to root_path
+      flash[:alert] = "自己的日記自己寫！"
+    end
+  end
+
+  def update
+    @diary = Diary.find(params[:id])
+    if @diary.update(diary_params)
+      redirect_to user_diaries_path
+      flash[:notice] = "日記順利更新完成囉！"
+    else
+      render :edit
+      flash[:alert] = "Oops，請您檢查日記欄位後再試一次 :P"
+    end
+  end
+
+  def destroy
+    @diary = Diary.find(params[:id])
+    @diary.destroy
+    redirect_to user_diaries_path
+    flash[:notice] = "日記已經刪除"
   end
 
   private
