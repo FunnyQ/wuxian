2b030102b93098941b42e8ca17ab6a3ed95337f2
FunnyQ <funnyq@funnyq.com>
2015-01-29 13:36:14 +0800

    [add] adding configurations

---
+++ config/deploy.rb
@@ -0,0 +1,62 @@
+# config valid only for Capistrano 3.1
+lock '3.1.0'
+
+set :application, 'wuxian-beta'
+set :repo_url, 'git@github.com:FunnyQ/wuxian.git'
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, '/home/deployer/wuxian-beta'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+before "deploy:assets:precompile", "bower:install"
+
+namespace :bower do
+
+  task :install do
+    on roles(:app) do
+      within release_path do
+        execute :rake, 'bower:install'
+      end
+    end
+  end
+
+end
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
+
+end
