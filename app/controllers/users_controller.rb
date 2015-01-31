b0e60a4973e090f74198213f96229f926d20d568
FunnyQ <funnyq@funnyq.com>
2015-01-31 18:15:59 +0800

    [add] done basic guestbook function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,17 @@ class UsersController < ApplicationController
 
     if @user != current_user
       @user.count_up!
+      write_in_guestbook(@user) if user_signed_in?
     end
 
+    @guestbook = @user.guestbooks.includes(:visitor).recent
     @status = @user.statuses.new
     @diaries = @user.diaries.get_latest(5)
+  end
+
+  def write_in_guestbook(user)
+    user.guestbooks.where(visitor_id: current_user.id).delete_all if user.guestbooks.find_by_visitor_id(current_user.id)
+    @user.guestbooks.create(visitor_id: current_user.id)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150129034139) do
+ActiveRecord::Schema.define(version: 20150131084650) do
 
   create_table "authorizations", force: :cascade do |t|
     t.string   "provider",   limit: 255
@@ -35,6 +35,13 @@ ActiveRecord::Schema.define(version: 20150129034139) do
   end
 
   add_index "diaries", ["user_id"], name: "index_diaries_on_user_id", using: :btree
+
+  create_table "guestbooks", force: :cascade do |t|
+    t.integer  "visitor_id", limit: 4
+    t.integer  "host_id",    limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
 
   create_table "statuses", force: :cascade do |t|
     t.text     "content",    limit: 65535
